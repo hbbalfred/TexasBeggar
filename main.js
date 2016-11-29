0ac39d60e11217703c220d166a678160c35e942c
hbbalfred <hbbalfred@gmail.com>
2016-11-29 11:59:00 +0800

    improve

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ Array.prototype.cycle = function(i) {
 		return this[0];
 	}
 	var idx = i % this.length;
-	return this[i];
+	return this[idx];
 };
 
 
@@ -88,6 +88,9 @@ Player.prototype.ready = function() {
 Player.prototype.addCoin = function(coin) {
 	this.coin += Math.abs(coin);
 };
+Player.prototype.addHandCard = function(card) {
+	this.handcards.push(card);
+};
 Player.prototype.bet = function(coin) {
 	if (this.coin < coin) {
 		console.warn(this.name +' has no enough coin');
@@ -96,27 +99,227 @@ Player.prototype.bet = function(coin) {
 
 	this.coin -= coin;
 };
-Player.prototype.addHandCard = function(card) {
-	this.handcards.push(card);
-};
 Player.prototype.fold = function() {
 	this.status = Player.FOLD;
 };
 
 
 
-function Game() {
+
+function Room() {
 	this.card = new Card();
-	this.flopCards = [];
-
-	this.players = {};
 	this.orders = [];
-
+	this._playerMap = null;
 	this.playing = false;
 
-	this.idxMang = -1;
-	this.bigMang = null;
-	this.smallMang = null;
+	this.flops = [];
+	this.bankerIndex = -1;
+	this.betIndex = -1;
+}
+Room.prototype.init = function(players) {
+	this._playerMap = {};
+	for (var name in players) {
+		this.orders.push(players[name]);
+		this._playerMap[name] = players[name];
+	}
+	this.orders.shuffle();
+};
+Room.prototype.newRound = function() {
+	if (this.playing) {
+		console.warn('room playing');
+		return;
+	}
+
+	this.playing = true;
+
+	this.flops.length = 0;
+	this.card.shuffle();
+	this.card.shuffle();
+
+	this.orders.forEach(function (player) {
+		player.ready();
+	}, this);
+
+	++this.bankerIndex;
+	this.betIndex = this.bankerIndex + 2;
+
+	var draw = function (player) {
+		player.addHandCard(this.card.next());
+	};
+	this.orders.forEach(draw, this);
+	this.orders.forEach(draw, this);
+};
+Room.prototype.endRound = function() {
+	this.playing = false;
+};
+Room.prototype.flop = function(n) {
+	n = n || 1;
+	for (var i = 0; i < n; ++i) {
+		this.flops.push(this.card.next());
+	}
+};
+Room.prototype.fold = function() {
+	if (this.betIndex === -1) {
+		console.warn('no player bet');
+		return;
+	}
+
+	this.orders.cycle(this.betIndex).fold();
+
+	this.__nextBet();
+};
+Room.prototype.bet = function(coin) {
+	if (this.betIndex === -1) {
+		console.warn('no player bet');
+		return;
+	}
+
+	this.orders.cycle(this.betIndex).bet(coin);
+
+	this.__nextBet();
+};
+Room.prototype.__nextBet = function() {
+	for (var i = 0; i < this.orders.length; ++i) {
+		var player = this.orders.cycle(this.betIndex + i + 1);
+		if (player.status !== Player.FOLD) {
+			this.betIndex = this.betIndex + i + 1;
+			return;
+		}
+	}
+
+	this.betIndex = -1;
+};
+Room.prototype.getPlayer = function(name) {
+	return this._playerMap[name];
+};
+Room.prototype.IsPlayerBanker = function(name) {
+	var player = this.orders.cycle(this.bankerIndex);
+	return player === this._playerMap[name];
+};
+Room.prototype.IsPlayerBigMang = function(name) {
+	var player = this.orders.cycle(this.bankerIndex + 1);
+	return player === this._playerMap[name];
+};
+Room.prototype.IsPlayerSmallMang = function(name) {
+	var player = this.orders.cycle(this.bankerIndex + 2);
+	return player === this._playerMap[name];
+};
+Room.prototype.IsPlayerBetNow = function(name) {
+	var player = this.orders.cycle(this.betIndex);
+	return player === this._playerMap[name];
+};
+Room.prototype.printPublic = function(verbose) {
+	var msg = '桌上牌面: {flops}\n';
+	msg += '桌上筹码: {allbet}\n';
+	msg += '轮到下注: {whobet}\n';
+
+	if (verbose) {
+		msg += '玩家情况:\n{infos}';	
+	}
+	
+
+	var flops = this.flops.map(function (card) {
+		return this.card.toRead(card);
+	}, this);
+
+	var whobet;
+	var allbet = 0;
+	var infos = this.orders.map(function (player) {
+		var info = player.name;
+		if (this.IsPlayerBanker(player.name)) {
+			info += '（庄）';
+		} else if (this.IsPlayerBigMang(player.name)) {
+			info += '（大）';
+		} else if (this.IsPlayerSmallMang(player.name)) {
+			info += '（小）';
+		}
+
+		if (this.IsPlayerBetNow(player.name)) {
+			whobet = player.name;
+		}
+
+		var bet = player.roundStartCoin - player.coin;
+		allbet += bet;
+
+		info += '\n拥有筹码: ' + player.coin;
+		info += '\n当前押注: ' + (player.status === Player.FOLD ? '（已弃牌）' : bet);
+		return info;
+	}, this);
+
+	msg = msg.replace('{flops}', flops.join('  '));
+	msg = msg.replace('{allbet}', allbet);
+	msg = msg.replace('{whobet}', whobet);
+	if (verbose) {
+		msg = msg.replace('{infos}', infos.join('\n'));	
+	}
+	console.log(msg);
+};
+Room.prototype.printPlayer = function(name) {
+	var player = this._playerMap[name];
+	var info = player.name;
+	if (this.IsPlayerBanker(player.name)) {
+		info += '（庄）';
+	} else if (this.IsPlayerBigMang(player.name)) {
+		info += '（大）';
+	} else if (this.IsPlayerSmallMang(player.name)) {
+		info += '（小）';
+	}
+
+	var handcards = player.handcards.map(function (card) {
+		return this.card.toRead(card);
+	}, this);
+
+	info += '\n筹码: ' + player.coin;
+	info += '\n手牌: ' + handcards.join('  ');
+	console.log(info);
+};
+Room.prototype.printEnd = function() {
+	var msg = '桌上牌面: {flops}\n';
+	msg += '桌上筹码: {allbet}\n';
+	msg += '玩家情况:\n{infos}';
+
+	var flops = this.flops.map(function (card) {
+		return this.card.toRead(card);
+	}, this);
+
+	var allbet = 0;
+
+	var infos = this.orders.map(function (player) {
+		var info = player.name;
+
+		if (player.status !== Player.FOLD) {
+			var handcards = player.handcards.map(function (card) {
+				return this.card.toRead(card);
+			}, this);
+
+			handcards = handcards.concat(flops);
+
+			info += ' ：' + handcards.join('  ');
+		} else {
+			info += ' ：（已弃牌）';
+		}
+
+		var bet = player.roundStartCoin - player.coin;
+		allbet += bet;
+		
+		return info;
+	}, this);
+
+	msg = msg.replace('{flops}', flops.join('  '));
+	msg = msg.replace('{allbet}', allbet);
+	msg = msg.replace('{infos}', infos.join('\n'));
+
+	console.log(msg);
+};
+
+
+
+
+
+
+function Game() {
+	this.players = {};
+	this.rooms = [];
 }
 Game.prototype.addPlayer = function (name, coin) {
 	if (this.players[name]) {
@@ -143,105 +346,48 @@ Game.prototype.removePlayer = function(name) {
 	delete this.players[name];
 	--this.players._length;
 };
+Game.prototype.createRoom = function() {
+	var tmp = this.rooms[0] = new Room();
+	tmp.init(this.players);
+	console.log('Room created');
+};
+
+
 Game.prototype.newRound = function() {
-	if (!this.players._length || this.players._length < 2) {
-		console.warn('no enough players');
-		return;
-	}
-	if (this.playing) {
-		console.warn('game is playing');
-		// return;
-	}
-
-	this.playing = true;
-
-	this.flopCards.length = 0;
-	this.card.shuffle();
-	this.card.shuffle();
-
-	var orders = this.orders = [];
-	for (var name in this.players) {
-		this.players[name].ready();
-		orders.push(this.players[name]);
-	}
-
-	orders.sort(function (a, b) {
-		if (a.name > b.name) return 1;
-		if (a.name < b.name) return -1;
-		return 0;
-	});
-
-	++this.idxMang;
-	this.bigMang = orders.cycle(this.idxMang);
-	this.smallMang = orders.cycle(this.idxMang + 1);
-	orders.shuffle();
-
-	for (var i = 0; i < orders.length; ++i) {
-		orders[i].addHandCard(this.card.next());
-	}
-	for (var i = 0; i < orders.length; ++i) {
-		orders[i].addHandCard(this.card.next());
+	this.rooms[0].newRound();
+	for (var i = 0; i < this.rooms[0].orders.length; ++i) {
+		this.rooms[0].printPlayer(this.rooms[0].orders[i].name);
 	}
 };
 Game.prototype.flop = function(n) {
-	for (var i = 0; i < n; ++i) {
-		this.flopCards.push(this.card.next());
-	}
+	this.rooms[0].flop(n);
+	this.rooms[0].printPublic();
 };
-Game.prototype.bet = function(name, coin) {
-	this.players[name].bet(coin);
+Game.prototype.bet = function(coin) {
+	this.rooms[0].bet(coin);
+	this.rooms[0].printPublic();
 };
-Game.prototype.fold = function(name) {
-	this.players[name].fold();
+Game.prototype.fold = function() {
+	this.rooms[0].fold();
+	this.rooms[0].printPublic();
+};
+Game.prototype.endRound = function() {
+	this.rooms[0].endRound();
+	this.rooms[0].printEnd();
+};
+Game.prototype.printRoom = function() {
+	this.rooms[0].printPublic();
 };
 
-Game.prototype.printPublic = function() {
-	var msg = '桌上牌面: {flops}\n';
-	msg += '桌上筹码: {allbet}\n';
-	msg += '玩家情况:\n{infos}';
 
-	var flops = this.flopCards.map(function (card) {
-		return this.card.toRead(card);
-	}, this);
-	
-
-	var allbet = 0;
-	var infos = this.orders.map(function (player) {
-		var info = player.name;
-		if (this.bigMang === player) {
-			info += '（大盲）'
-		} else if (this.smallMang === player) {
-			info += '（小盲）'
-		}
-
-		var bet = player.roundStartCoin - player.coin;
-		allbet += bet;
-
-		info += '\n拥有筹码: ' + player.coin;
-		info += '\n当前押注: ' + (player.status === Player.FOLD ? '（已弃牌）' : bet);
-		return info;
-	}, this);
-
-	msg = msg.replace('{flops}', flops.join('  '));
-	msg = msg.replace('{allbet}', allbet);
-	msg = msg.replace('{infos}', infos.join('\n'));
-	return msg;
-};
-Game.prototype.printPlayer = function(name) {
-	var player = this.players[name];
-	var info = player.name;
-	if (this.bigMang === player) {
-		info += '（大盲）'
-	} else if (this.smallMang === player) {
-		info += '（小盲）'
-	}
-
-	var handcards = player.handcards.map(function (card) {
-		return this.card.toRead(card);
-	}, this);
-
-	info += '\n筹码: ' + player.coin;
-	info += '\n手卡: ' + handcards.join('  ');
-	return info;
-};
 var game = new Game();
+
+
+game.addPlayer('colin', 1000);
+game.addPlayer('tom', 1000);
+game.addPlayer('hbb', 1000);
+
+game.createRoom();
+game.newRound();
+
+game.bet(50);
